c8c3b8ac10b003d06506a1a1ac119bb25c304749
RonanAlmeida <ronancalmeida@hotmail.com>
2021-02-07 08:58:46 -0500

    added

--- flask-backend/api/views.py
+++ flask-backend/api/views.py
@@ -67,9 +67,30 @@ def find_ticker_counter(subreddit):
     # new_sorted = {k: v for k, v in sorted(improv.items(), key=lambda item: item[1])}
     new_sorted = dict(sorted(improv.items(), key=operator.itemgetter(1),reverse=True))
 
+    arr_keys=[]
+    arr_vals=[]
     for key in new_sorted:
         hashArray.append({"ticker":key,"Count":new_sorted[key]})
-    return hashArray,sub_array[0:40]
+        arr_keys.append(key)
+        arr_vals.append(new_sorted[key])
+
+    final={}
+    final['keys'] = arr_keys
+    final['vals'] = arr_vals
+
+    colors_arr =  ["#FF6384", "#36A2EB", "#FFCE56","#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
+"#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
+"#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
+"#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,
+"#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,
+"#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,
+"#935b6d" ,"#916988" ,"#513d98" ,"#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
+"#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
+"#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
+"#5be4f0", "#57c4d8", "#a4d17a", "#225b8",]
+
+    final['colors']=colors_arr
+    return hashArray,sub_array[0:40],final
 
 
 # Returns the Tickers and the ticker hounders
