def24f471d560044cdbee133f4b40d4a0a53e1a0
RonanAlmeida <ronancalmeida@hotmail.com>
2021-02-06 19:14:27 -0500

    added flask react

--- stock-market-frontend/src/App.js
+++ stock-market-frontend/src/App.js
@@ -1,9 +1,22 @@
-import logo from "./logo.svg";
+import {useEffect,useState} from 'react'
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import "semantic-ui-css/semantic.min.css";
 
 function App() {
+  const [ticks, setTicks] = useState([])
+
+  useEffect(() => {
+    fetch('/get_stock_analysis').then(response => 
+      response.json().then(data => {
+        // setTicks(data)
+        // console.log(data)
+        setTicks(data.tickers)
+      }))
+
+  }, [])
+  console.log(ticks)
+
   return(
   <div className="App">
     <HomePage></HomePage>
